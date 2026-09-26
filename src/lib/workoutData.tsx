2a211a07cac47ea560ed031986356766433e425a
mehedynoman11 <mehedynoman11@gmail.com>
2026-09-26 22:54:15 +0600

    api url change

--- src/lib/workoutData.tsx
+++ src/lib/workoutData.tsx
@@ -1,5 +1,5 @@
 export const getData = async () => {
-    const res = await fetch(`https://api.abcz.workers.dev/api/fitlog`);
+    const res = await fetch(`https://api.api-store.workers.dev/api/fitlog`);
     if (!res.ok) {
         throw new Error("Couldn't fetch data!");
     }
